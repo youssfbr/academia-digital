a2b0dfbd1d97945dc2f3ccdbef660c182a19ae30
Alisson Youssf <youssfbr@gmail.com>
2023-03-26 22:14:28 -0300

    Projeto criado

---
+++ src/main/java/com/github/youssfbr/academiadigital/AcademiaDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.academiadigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/youssfbr/academiadigital/AcademiaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.academiadigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
